e487bfd6aff5bf4d241e2ac12713e77b60cec62f
manhnd <nguyenducmanh1888@gmail.com>
2025-04-20 20:38:16 +0700

    Home: update responsive

--- src/components/ForecastByDate.tsx
+++ src/components/ForecastByDate.tsx
@@ -38,19 +38,22 @@ const ForecastByDate: React.FC<ForecastByDateProps> = ({
     <>
       <h2 className="text-lg text-gray-800 font-medium mb-4">5-Day Forecast</h2>
       {isLoading ? (
-        <div className="min-h-[240px] flex items-center justify-center bg-white rounded-lg p-6 shadow-lg">
+        <div className="min-h-[240px] flex items-center justify-center rounded-lg p-6  bg-white md:bg-transparent shadow-lg md:shadow-transparent">
           <FaSpinner className="icon-spin text-gray-600" />
         </div>
       ) : (
         forecastData && (
-          <div className="bg-white rounded-lg p-6 shadow-lg">
+          <div className="bg-white rounded-lg shadow-lg p-6 md:p-0 md:shadow-transparent md:bg-transparent">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
               {Object.entries(forecastGroupByDate).map(([date, items]) => {
                 const label = isToday(new Date(items[0].dt * 1000))
                   ? 'Today'
                   : date;
                 return (
-                  <div key={date}>
+                  <div
+                    key={date}
+                    className="bg-transparent md:bg-white md:p-4 md:shadow-lg md:rounded-lg"
+                  >
                     <span className="text-gray-500">{label}</span>
                     {items.map((item: ForecastItem) => (
                       <WeatherRow key={item.dt} {...item} />
